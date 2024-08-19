d2e78cf09da7b5eb976015ed672ababaaf87c743
SERRE Loic <loic.serre@etu.univ-amu.fr>
2024-08-20 00:13:05 +0200

    Ajout de la scène threejs à la page skills ainsi que le css

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import '../app/globals.css';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
+import SkillScene from '@/components/skillscene';
 
 export default function Skills() {
     const [theme, setTheme] = useState('gradient');
@@ -17,13 +18,14 @@ export default function Skills() {
 
     return (
         <main className={`flex min-h-screen flex-col items-center justify-between p-24 transition-colors ${themeClass}`}>
-
             <Header toggleTheme={toggleTheme} theme={theme} />
-
-            
-
+            <div className="flex flex-col items-center justify-center h-full">
+                <h1 className="text-4xl font-bold">Skills</h1>
+                <p className="text-lg text-center">Cliquez sur un logo pour en savoir plus</p>
+                
+                <SkillScene />
+            </div>
             <Footer />
-            
         </main>
     );
 }
