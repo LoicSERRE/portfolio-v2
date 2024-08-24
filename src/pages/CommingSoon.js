2ff8252bb8bb11c375d6046eb893a17d40e4cd51
SERRE Loic <loic.serre@etu.univ-amu.fr>
2024-08-24 03:28:55 +0200

    Correction CommingSoon

--- src/pages/CommingSoon.js
+++ src/pages/CommingSoon.js
@@ -6,27 +6,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 
 export default function ComingSoon() {
-    const [theme, setTheme] = useState('gradient');
-
-    const toggleTheme = () => {
-        const themes = ['default', 'dark', 'gradient'];
-        const currentIndex = themes.indexOf(theme);
-        const nextIndex = (currentIndex + 1) % themes.length;
-        setTheme(themes[nextIndex]);
-    };
-
-    const themeClass = theme === 'dark' ? 'bg-black text-white' : theme === 'gradient' ? 'gradient-theme' : 'bg-white text-black';
-
     return (
-        <main className={`flex min-h-screen flex-col items-center justify-between p-24 transition-colors ${themeClass}`}>
-            <Header toggleTheme={toggleTheme} theme={theme} />
+        <main className={`flex min-h-screen flex-col items-center justify-between p-24 transition-colors`}>
             <div className="flex flex-col items-center justify-center flex-1">
-                <FontAwesomeIcon icon={faExclamationTriangle} className="text-red-600 text-6xl mb-4" />
+                <FontAwesomeIcon icon={faExclamationTriangle} className="text-red-600 text-8xl mb-4" />
                 <h1 className="text-4xl font-bold mb-4">Fonctionnalité à venir</h1>
                 <p className="text-lg mb-8">Je travaille pour vous apporter cette fonctionnalité. Restez à l'écoute !</p>
                 <div className="text-gray-500">Cette fonctionnalité n'est pas encore disponible.</div>
             </div>
-            <Footer />
         </main>
     );
 }
