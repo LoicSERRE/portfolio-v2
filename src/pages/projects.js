a25acec5dc49c5dbf620bde14bffa3cb579108f8
SERRE Loic <loic.serre@etu.univ-amu.fr>
2024-08-27 23:08:12 +0200

    Légère correction sur la liste des projets (mise en page, couleur...)

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -19,8 +19,8 @@ export default function Projects() {
     const [projects, setProjects] = useState([
         {
             id: 1,
-            title: "Projet annuel de 3ème année de BUT",
-            description: "Développement d'un PoC permettant de gérer une cartographie de port commercial.",
+            title: "PoC Cartographie Portuaire",
+            description: "Développement d'un PoC dans le cadre de ma dernière années de BUT Informatique, ce projet permet de gérer une cartographie de port commercial.",
             technologies: ["JavaScript", "Node.js", "Express", "React", "Docker"],
             isOnline: true,
             link: "/projects/projetannuel",
@@ -97,21 +97,19 @@ export default function Projects() {
     const linkHoverClass = 'hover:text-teal-300 focus-visible:text-teal-300';
 
     return (
-        <main className={`flex min-h-screen flex-col items-center justify-between p-24 transition-colors ${themeClass}`}>
-            <Header toggleTheme={toggleTheme} theme={theme} />
+        <main className={`flex min-h-screen flex-col items-center justify-between px-4 py-24 transition-colors ${themeClass}`}>            <Header toggleTheme={toggleTheme} theme={theme} />
 
-            <div className="w-full max-w-4xl mx-auto space-y-8">
+            <div className="w-full mx-auto space-y-8 project-card">
                 {projects.map((project) => (
-                    <div key={project.id} className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover:list:opacity-50">
-                        <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-                        <div className="z-10 sm:order-2 sm:col-span-6">
+                    <div key={project.id} className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover:list:opacity-30">
+                        <div className="absolute -inset-x-4 -inset-y-4 z-0 rounded-md transition lg:-inset-x-6 group-hover:bg-slate-800/50 group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] group-hover:drop-shadow-lg"></div>                        <div className="z-10 sm:order-2 sm:col-span-6">
                             <h3>
                                 <Link href={project.link} target="_blank" rel="noreferrer noopener" legacyBehavior>
                                     <a
-                                        className={`inline-flex items-baseline font-medium leading-tight ${textColorClass} ${linkHoverClass} group/link text-base`}
+                                        className={`inline-flex items-baseline font-medium leading-tight ${textColorClass} ${linkHoverClass} group/link text-base ml-4`}
                                         aria-label={`${project.title} (opens in a new tab)`}
                                     >
-                                        <span className="relative">
+                                        <span className="relative mt-2">
                                             <span>
                                                 {project.title}
                                                 <span className="inline-block">
@@ -137,7 +135,7 @@ export default function Projects() {
 
                             {/* Bloc pour indiquer la disponibilité en ligne */}
                             {project.isOnline && (
-                                <div className="mt-1 text-xs text-green-500 font-semibold flex items-center">
+                                <div className="mt-1 text-xs text-green-500 font-semibold flex items-center ml-4">
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 20 20"
@@ -154,18 +152,18 @@ export default function Projects() {
                                 </div>
                             )}
 
-                            <h4 className="mt-2 text-sm text-white">{project.description}</h4>
+                            <h4 className="mt-2 text-sm text-white ml-4 mr-2">{project.description}</h4>
 
                             {project.githubStars !== 0 && (
                                 <Link href={project.link} target="_blank" rel="noreferrer noopener" legacyBehavior>
                                     <a
-                                        className={`relative mt-2 inline-flex items-center text-sm font-medium ${textColorClass} ${linkHoverClass}`}
+                                        className={`relative ml-4 mt-2 inline-flex items-center text-sm font-medium ${textColorClass} ${linkHoverClass}`}
                                         aria-label={`${project.githubStars} stars on GitHub (opens in a new tab)`}
                                     >
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             viewBox="0 0 20 20"
-                                            fill="currentColor"
+                                            fill="yellow"
                                             className={`mr-1 h-3 w-3 ${textColorClass}`}
                                             aria-hidden="true"
                                         >
@@ -175,7 +173,7 @@ export default function Projects() {
                                                 clipRule="evenodd"
                                             />
                                         </svg>
-                                        <span>{project.githubStars}</span>
+                                        <span className='text-white'>{project.githubStars}</span>
                                     </a>
                                 </Link>
                             )}
@@ -183,7 +181,7 @@ export default function Projects() {
                             <ul className="mt-2 flex flex-wrap" aria-label="Technologies used:">
                                 {project.technologies.map((tech, index) => (
                                     <li key={index} className="mr-1.5 mt-2">
-                                        <div className={`flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300`}>
+                                        <div className={`flex items-center rounded-full bg-teal-400/10 px-3 py-1 ml-2 text-xs font-medium leading-5 text-teal-300`}>
                                             {tech}
                                         </div>
                                     </li>
@@ -192,11 +190,10 @@ export default function Projects() {
                         </div>
                         <img
                             alt={project.title}
-                            loading="lazy"
                             width="500"
                             height="500"
                             decoding="async"
-                            className="p-2 rounded-[20px] border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                            className="p-2 rounded-[20px] border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1 z-10"
                             src={project.image}
                         />
                     </div>
