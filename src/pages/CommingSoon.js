ec54b4cb4201e264e71e33904a5dae9685cf87e2
SERRE Loic <loic.serre@etu.univ-amu.fr>
2024-08-29 00:21:06 +0200

    Correction des caractères spéciaux

--- src/pages/CommingSoon.js
+++ src/pages/CommingSoon.js
@@ -8,8 +8,8 @@ export default function ComingSoon() {
             <div className="flex flex-col items-center justify-center flex-1">
                 <FontAwesomeIcon icon={faExclamationTriangle} className="text-red-600 text-8xl mb-4" />
                 <h1 className="text-4xl font-bold mb-4">Fonctionnalité à venir</h1>
-                <p className="text-lg mb-8">Je travaille pour vous apporter cette fonctionnalité. Restez à l'écoute !</p>
-                <div className="text-gray-500">Cette fonctionnalité n'est pas encore disponible.</div>
+                <p>Je travaille pour vous apporter cette fonctionnalité. Restez à l&apos;écoute!</p>
+                <div className="text-gray-500">Cette fonctionnalité n&apos;est pas encore disponible.</div>
             </div>
         </main>
     );
