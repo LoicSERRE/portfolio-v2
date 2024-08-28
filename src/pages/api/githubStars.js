5ce0dc22b0d3f7673c239879a92b209c0ece1b79
SERRE Loic <loic.serre@etu.univ-amu.fr>
2024-08-29 00:22:12 +0200

    Modification du fonctionnement de la récupération des étoiles github

--- src/pages/api/githubStars.js
+++ src/pages/api/githubStars.js
@@ -2,27 +2,40 @@ import { apiLimiter } from '../../middleware/rateLimit';
 
 export default async function handler(req, res) {
     apiLimiter(req, res, async () => {
-        const { repo } = req.query;
+        const { repos } = req.query; // Repos est un string de dépôts séparés par des virgules
         const token = process.env.GITHUB_TOKEN;
 
         if (!token) {
             return res.status(500).json({ error: 'GitHub token is not set' });
         }
 
+        if (!repos) {
+            return res.status(400).json({ error: 'No repositories provided' });
+        }
+
+        const repoList = repos.split(','); // Convertit la chaîne de caractères en tableau
+
         try {
-            const response = await fetch(`https://api.github.com/repos/LoicSERRE/${repo}`, {
-                headers: {
-                    'Authorization': `token ${token}`
-                }
+            const fetchPromises = repoList.map(repo => {
+                return fetch(`https://api.github.com/repos/LoicSERRE/${repo}`, {
+                    headers: {
+                        'Authorization': `token ${token}`
+                    }
+                });
             });
 
-            if (!response.ok) {
-                return res.status(response.status).json({ error: 'Failed to fetch GitHub stars' });
-            }
+            const responses = await Promise.all(fetchPromises);
 
-            const data = await response.json();
-            const stars = parseInt(data.stargazers_count, 10);
-            res.status(200).json({ stars });
+            const results = await Promise.all(responses.map(async response => {
+                if (!response.ok) {
+                    return { error: `Failed to fetch GitHub stars for ${response.url}` };
+                }
+                const data = await response.json();
+                return { [data.name]: parseInt(data.stargazers_count, 10) };
+            }));
+
+            const starsData = results.reduce((acc, curr) => ({ ...acc, ...curr }), {});
+            res.status(200).json(starsData);
         } catch (error) {
             res.status(500).json({ error: 'Internal Server Error' });
         }
