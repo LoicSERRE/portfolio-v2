bdde81dc49bafd75b0230c13b7e922bb93bb5465
SERRE Loïc <loic.serre@etu.univ-amu.fr>
2024-10-05 16:06:34 +0200

    Correction pour déploiement

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['upload.wikimedia.org', 'cdn.icon-icons.com', 'logowik.com', 'global.discourse-cdn.com'],
-    },
-  };
-  
-  export default nextConfig;
+  basePath: '/portfolio',
+  images: {
+    domains: ['upload.wikimedia.org', 'cdn.icon-icons.com', 'logowik.com', 'global.discourse-cdn.com'],
+  },
+};
+
+export default nextConfig;
