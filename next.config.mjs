5841024c179be32f0f632ac512370ea21b5fbf39
SERRE Loïc <loic.serre@etu.univ-amu.fr>
2024-10-12 00:45:33 +0200

    Retire le /portfolio de l'url de base

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/portfolio',
   images: {
     domains: ['upload.wikimedia.org', 'cdn.icon-icons.com', 'logowik.com', 'global.discourse-cdn.com'],
   },
